412f88168f9517e306533a55970c43a8bc42c5a2
Carlos Mario Cervantes Ariza <ccervantes83@uan.edu.co>
2022-04-24 17:55:51 -0500

    'Evitar_prefijos/sufijos_innecesarios'

--- VideoStoreLab/src/Movie.java
+++ VideoStoreLab/src/Movie.java
@@ -3,23 +3,23 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
-    private String _title;
-    private int _priceCode;
+    private String title;
+    private int priceCode;
 
     public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
+        this.title = title;
+        this.priceCode = priceCode;
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return priceCode;
     }
 
-    public void setPriceCode(int _priceCode) {
-        this._priceCode = _priceCode;
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
     }
 
     public String getTitle() {
-        return _title;
+        return title;
     }
 }
